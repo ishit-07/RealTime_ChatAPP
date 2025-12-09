2b6284f81a2434cf81e1a94e6795f73fca5ba06a
Ishit Verma <ishitverma2602@gmail.com>
2025-12-09 12:02:14 +0530

    Project Completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,25 +4,19 @@ import { connectDB } from "../src/lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "../src/lib/socket.js";
-import path from "path";
-
-// we have to put the file extenstion for the local file because we are using type: module in the package.json
 import authRoutes from "../src/routes/auth.routes.js";
 import messageRoutes from "../src/routes/message.routes.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-// CORS configuration to allow requests from the frontend
-// and to allow credentials (cookies) to be sent with requests
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chatty-nu-bay.vercel.app/"],
     credentials: true,
   })
 );
@@ -30,14 +24,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
 server.listen(PORT, () => {
-  console.log("Server is running on port: " + PORT);
+  console.log("Server running on port: " + PORT);
   connectDB();
 });
