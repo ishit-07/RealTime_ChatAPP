f045457aea4e96b4ccfdf03613b59ba514007772
Ishit Verma <ishitverma2602@gmail.com>
2025-12-09 12:13:27 +0530

    Project Completed

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-
-  // this is for cookies to be sent with requests
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
