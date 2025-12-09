74bdb91dbe39a04e228e2643ba9f21e8eae293c4
Ishit Verma <ishitverma2602@gmail.com>
2025-12-09 13:58:35 +0530

    Project Completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -19,6 +19,7 @@ app.use(
     origin: ["http://localhost:5173", "https://chatty-delta-wine.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
